2a2594ee74ac9e3845a984df5090aafca931b402
cDev-Creator <mccoyng170@gmail.com>
2023-03-04 23:49:27 -0600

    reusing the BlogList component by filtering by  author

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'How to use PHP like a pro', body: 'lorem ipsum...', author: 'John Marks', id: 1 },
         { title: 'Why your approach to web development is wrong!', body: 'lorem ipsum...', author: 'Steven Odes', id: 2 },
-        { title: 'My favorite react tips and tricks', body: 'lorem ipsum...', author: 'Mellisa Scott', id: 3 }
+        { title: 'My favorite react tips and tricks', body: 'lorem ipsum...', author: 'Mellisa Scott', id: 3}, 
+        {title:  'Get started with MySQL', body: 'lorem ipsum...', author: 'John Marks', id: 4 }
       ])
+
     return(
         <div className = 'home'>
-            {blogs.map((blog) =>
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            
-            </div>
-            )}
+            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'John Marks')} title="Blogs by John Marks"/>
         </div>
     )
 }
+
 
 export default Home;
