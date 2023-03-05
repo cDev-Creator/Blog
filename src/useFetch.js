833ba9a59e868ad48b6b887dcecca568dc46240d
cDev-Creator <mccoyng170@gmail.com>
2023-03-05 16:35:49 -0600

    custom reusable hook for throwing  errors

---
+++ src/useFetch.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react';
+
+/* custom hooks must start with use */
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true); 
+    const [error, setError] = useState(null); 
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                console.log(res);
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            })
+           /*  catches any network error, can't connect to server */
+            .catch(err => {
+                setIsLoading(false);
+                setError(err.message);
+            })
+    }, [url])
+
+    return {data, isLoading, error}
+}
+
+export default useFetch;
