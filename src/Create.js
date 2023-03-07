80cdce7405eb07f226692864ad203bf348ab2256
cDev-Creator <mccoyng170@gmail.com>
2023-03-07 12:10:40 -0600

    add new blogs to json db

--- src/Create.js
+++ src/Create.js
@@ -3,11 +3,28 @@ import { useState } from "react";
 const Create = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
-    const [body, setBody] = useState('Rat Baby');
+    const [body, setBody] = useState('');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author}
+        console.log(blog);
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method:'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
+    }
     return ( 
        <div className="create">
         <h2>Add New Blog</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Blog title:</label>
             <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
 
@@ -20,7 +37,8 @@ const Create = () => {
             <label>Blog Body:</label>
             <textarea required value={body} onChange={(e)=>setBody(e.target.value)}> </textarea>
 
-            <button>Add Blog</button>
+            {!isPending && <button>Add Blog</button>}
+            {isPending && <button>Adding Blog...</button>}
             <p>{title}</p>
             <p>{body}</p>
             <p>{author}</p>
